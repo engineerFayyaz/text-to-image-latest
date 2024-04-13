2d70968c2d78291661a2f7ee0fabeb52dd19a590
codesinc-repo <codes.inc.com@gmail.com>
2024-04-13 14:13:21 +0500

    code updated |new features ADDED

--- src/components/ImageGenerator/ImageGenerator.jsx
+++ src/components/ImageGenerator/ImageGenerator.jsx
@@ -12,57 +12,50 @@ import {
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Header from "../Header";
-import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { IconContext } from "react-icons";
+import { AiOutlineCheck } from "react-icons/ai";
 import CartModal from "../Cart";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { Link, useNavigate } from "react-router-dom";
-import {getFirestore, collection, addDoc} from "firebase/firestore"
-// import { FaShoppingCart } from 'react-icons/fa';
+import { useNavigate } from "react-router-dom";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+
 const ImageGenerator = () => {
   const [originalImageUrl, setOriginalImageUrl] = useState("");
   const [generatedImageUrls, setGeneratedImageUrls] = useState([]);
   const [loading, setLoading] = useState(false);
   const [progress, setProgress] = useState(0);
   const [inspirationText, setInspirationText] = useState("");
-  const [colorSaturation, setColorSaturation] = useState(0);
-  const [brightness, setBrightness] = useState(0);
-  const [contrast, setContrast] = useState(0);
   const [posterSize, setPosterSize] = useState("A4");
-  const [numImages, setNumImages] = useState(1);
+  const [numImages] = useState(1);
   const [isImageGenerated, setIsImageGenerated] = useState(false);
   const inputRef = useRef(null);
-  const numImagesRef = useRef(null);
   const canvasRef = useRef(null);
   const [cartItems, setCartItems] = useState([]);
   const [showModal, setShowModal] = useState(false);
+  const [selectedStyle, setSelectedStyle] = useState("vivid"); // Default style'
   const auth = getAuth();
   const navigate = useNavigate();
+
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-      } else {      
+      if (!user) {
+        toast.error("Please log in to generate images.");
+        setTimeout(() => {
+          navigate("/authentication");
+        }, 1000);
       }
     });
 
     // Clean up subscription on unmount
     return () => unsubscribe();
-  }, [auth]);
+  }, [auth, navigate]);
 
   const imageGenerator = async () => {
-
-
     const currentUser = auth.currentUser;
-    if (!currentUser) {
-      toast.error("Please log in to generate images.");
+    if (!currentUser) return;
 
-      setTimeout(() => {
-        navigate("/authentication")
-    },1000)
-      return;
-    
-    }
-    
     if (!inputRef.current || !inputRef.current.value) {
       toast.error("Please enter a description.");
       return;
@@ -72,22 +65,19 @@ const ImageGenerator = () => {
     setProgress(0);
 
     try {
-      const response = await fetch(
-        "https://api.openai.com/v1/images/generations",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization:
-              "Bearer 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",
-            "User-Agent": "Chrome",
-          },
-          body: JSON.stringify({
-            prompt: inputRef.current.value,
-            n: numImages,
-          }),
-        }
-      );
+      const response = await fetch("https://api.openai.com/v1/images/generations", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer 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",
+          "User-Agent": "Chrome",
+        },
+        body: JSON.stringify({
+          prompt: inputRef.current.value,
+          n: numImages,
+          style: selectedStyle,
+        }),
+      });
 
       if (response.ok) {
         const data = await response.json();
@@ -96,14 +86,13 @@ const ImageGenerator = () => {
         setOriginalImageUrl(imageUrls[0]);
         setGeneratedImageUrls(imageUrls);
         setIsImageGenerated(true);
-        // console.log("setGeneratedImageUrls", imageUrls.slice(1));
 
         const db = getFirestore();
         const imageCollectionRef = collection(db, "images");
         imageUrls.forEach(async (imageUrl) => {
           try {
             await addDoc(imageCollectionRef, { imageUrl });
-            toast.sucess("Image URL stored in Firestore:", imageUrl);
+            toast.success("Image URL stored in Firestore: " + imageUrl);
           } catch (error) {
             console.error("Error storing image URL in Firestore:", error);
           }
@@ -130,35 +119,17 @@ const ImageGenerator = () => {
 
   const handleAddToCart = (imageUrl) => {
     if (!cartItems.includes(imageUrl)) {
-      console.log("Image URL fetched successfully:", imageUrl);
       setCartItems([...cartItems, imageUrl]);
       handleShowModal(); // Show the modal after adding the item to the cart
     } else {
       toast.info("Item is already in the cart.");
     }
   };
+
   const handleRemoveFromCart = (imageUrl) => {
     const updatedCart = cartItems.filter((item) => item !== imageUrl);
     setCartItems(updatedCart);
   };
-  // const handleRemoveFromCart = (index) => {
-  //   const updatedCart = cartItems.filter((item, i) => i !== index);
-  //   setCartItems(updatedCart);
-  // };
-
-  useEffect(() => {
-    if (!loading) {
-      setTimeout(() => {
-        setProgress(0);
-      }, 3000);
-    }
-  }, [loading]);
-
-  useEffect(() => {
-    if (originalImageUrl && generatedImageUrls.length > 0) {
-      drawImages();
-    }
-  }, [originalImageUrl, generatedImageUrls]);
 
   const drawImages = () => {
     const canvas = canvasRef.current;
@@ -172,9 +143,24 @@ const ImageGenerator = () => {
     generatedImageUrls.forEach((url, index) => {
       const img = new Image();
       img.onload = () => {
-        const x = (index % 2) * canvas.width;
-        const y = Math.floor(index / 2) * canvas.height;
-        context.drawImage(img, x, y, canvas.width, canvas.height);
+        const aspectRatio = img.width / img.height;
+        let width, height, x, y;
+
+        // Determine size based on selected posterSize
+        if (posterSize === "portrait") {
+          // Portrait orientation
+          width = aspectRatio > 1 ? canvas.width / 2 : canvas.width;
+          height = aspectRatio > 1 ? canvas.height : canvas.height / 2;
+        } else {
+          // Landscape orientation
+          width = aspectRatio > 1 ? canvas.width : canvas.width / 2;
+          height = aspectRatio > 1 ? canvas.height / 2 : canvas.height;
+        }
+
+        x = (index % 2) * (canvas.width / 2);
+        y = Math.floor(index / 2) * (canvas.height / 2);
+
+        context.drawImage(img, x, y, width, height);
 
         // Increment the counter
         imagesLoaded++;
@@ -189,112 +175,29 @@ const ImageGenerator = () => {
     });
   };
 
-  const handleInspirationTextChange = (text) => {
-    setInspirationText(text);
-  };
 
-  const handleColorSaturationChange = (value) => {
-    setColorSaturation(value);
-  };
-
-  const handleBrightnessChange = (value) => {
-    setBrightness(value);
-  };
-
-  const handleContrastChange = (value) => {
-    setContrast(value);
-  };
-
-  const handlePosterSizeChange = (size) => {
-    setPosterSize(size);
-  };
-
-  const handleNumImagesChange = (value) => {
-    if (value === 4) {
-      toast.error(
-        "Please upgrade your package to premium to generate more than 3 images."
-      );
-    } else {
-      setNumImages(value);
-      numImagesRef.current = value;
+  useEffect(() => {
+    if (originalImageUrl && generatedImageUrls.length > 0) {
+      drawImages();
     }
-  };
-
-  useEffect(() => {
-    const disableRightClick = (e) => {
-      e.preventDefault();
-      toast.error("Right-clicking is disabled for this website.");
-    };
-
-    // Disable right-click context menu
-    window.addEventListener("contextmenu", disableRightClick);
-
-    return () => {
-      // Cleanup
-      window.removeEventListener("contextmenu", disableRightClick);
-    };
-  }, []);
-
-  useEffect(() => {
-    const disableScreenshots = (e) => {
-      e.preventDefault();
-      toast.error("Screenshotting is disabled for this website.");
-    };
-
-    // Disable screenshotting
-    window.addEventListener("keydown", (e) => {
-      if ((e.ctrlKey || e.metaKey) && (e.key === "s" || e.key === "S")) {
-        // Ctrl + S
-        disableScreenshots(e);
-      } else if ((e.ctrlKey || e.metaKey) && (e.key === "c" || e.key === "C")) {
-        // Ctrl + C
-        disableScreenshots(e);
-      }
-    });
-  });
-
-  useEffect(() => {
-    // Select all elements with data-bs-toggle="tooltip"
-    const tooltipTriggerList = document.querySelectorAll(
-      '[data-bs-toggle="tooltip"]'
-    );
-
-    // Convert NodeList to an array and map over it to create tooltips
-    const tooltipList = Array.from(tooltipTriggerList).map(
-      (tooltipTriggerEl) => {
-        return new window.bootstrap.Tooltip(tooltipTriggerEl);
-      }
-    );
-
-    // Cleanup function to destroy tooltips when component unmounts
-    return () => {
-      tooltipList.forEach((tooltip) => {
-        tooltip.dispose();
-      });
-    };
-  }, []); // Run only once when component mounts
+  }, [originalImageUrl, generatedImageUrls]);
 
   return (
     <>
-      <Header
-        cartItems={cartItems}
-        handleRemoveFromCart={handleRemoveFromCart}
-      />
+      {/* Header component */}
+      <Header cartItems={cartItems} handleRemoveFromCart={handleRemoveFromCart} />
       <ToastContainer />
       <Container fluid>
-        <Row className="image-generator mt-20 p-3 gap-4 gap-md-0 align-items-center justify-content-center  ">
+        <Row className="image-generator mt-20 p-3 gap-4 gap-md-0 align-items-center justify-content-center">
           <Col md={8}>
             <div className="live-preview text-center">
               <h1 className="text-center">
                 {/* Convert Your Imagination into Reality with AI */}
               </h1>
               <ProgressBar animated now={progress} label={`${progress}%`} />
-              {/* Add this line */}
               {loading && <p className="pb-0">Please Wait...</p>}
-              {/* Display the grid of images */}
               {isImageGenerated && (
                 <div className="image-grid">
-                  {/* <img src={originalImageUrl} alt="Original" className="original-image" /> */}
                   {generatedImageUrls.slice(0, numImages).map((url, index) => (
                     <div key={index} className="image-container">
                       <img
@@ -305,8 +208,7 @@ const ImageGenerator = () => {
                       <span>
                         <Button
                           title="Add to Cart"
-                          data-bs-toggle="tooltip"
-                          onClick={() => handleAddToCart(url)} // Pass the image URL to handleAddToCart
+                          onClick={() => handleAddToCart(url)}
                         >
                           <FontAwesomeIcon icon={faCartShopping} />
                         </Button>
@@ -315,44 +217,108 @@ const ImageGenerator = () => {
                   ))}
                 </div>
               )}
-
-              {/* Use canvas to edit images */}
-              <canvas ref={canvasRef} className="bg-grey-700  rounded-4 mt-4" />
+              <canvas ref={canvasRef} className="bg-grey-700 rounded-4 mt-4" />
             </div>
-            <div className="generate-boxs d-flex align-items-center flex-column justify-content-center  ">
+            <Form.Group className="d-flex justify-content-between align-items-center">
+              <Form.Check
+                type="radio"
+                label="Portrait"
+                name="posterSize"
+                value="portrait"
+                checked={posterSize === "portrait"}
+                onChange={() => setPosterSize("portrait")}
+              />
+              <Form.Check
+                type="radio"
+                label="Landscape"
+                name="posterSize"
+                value="landscape"
+                checked={posterSize === "landscape"}
+                onChange={() => setPosterSize("landscape")}
+              />
+            </Form.Group>
+
+
+            <div className="generate-boxs d-flex align-items-center flex-column justify-content-center">
               <Card className="mt-2 w-100 w-md-50 shadow-none border-0 text-center">
                 <Card.Body>
                   <h2>Choose Your Style</h2>
                   <Form.Group className="d-flex align-items-center justify-content-center gap-4">
-                    <Form.Check
-                      type="radio"
-                      label="Portrait"
-                      name="posterSize"
-                      checked={posterSize === "Portrait"}
-                      onChange={() => handlePosterSizeChange("Portrait")}
-                    />
-                    <Form.Check
-                      type="radio"
-                      label="Landscape"
-                      name="posterSize"
-                      checked={posterSize === "Landscape"}
-                      onChange={() => handlePosterSizeChange("Landscape")}
-                    />
+                    <IconContext.Provider value={{ size: "2rem", style: { verticalAlign: "middle" } }}>x
+                      <Form.Check
+                        type="radio"
+                        label={
+                          <div>
+                            <AiOutlineCheck /> Vivid
+                          </div>
+                        }
+                        name="style"
+                        value="vivid"
+                        checked={selectedStyle === "vivid"}
+                        onChange={(e) => setSelectedStyle(e.target.value)}
+                      />
+                      <Form.Check
+                        type="radio"
+                        label={
+                          <div>
+                            <AiOutlineCheck /> Natural
+                          </div>
+                        }
+                        name="style"
+                        value="natural"
+                        checked={selectedStyle === "natural"}
+                        onChange={(e) => setSelectedStyle(e.target.value)}
+                      />
+                      <Form.Check
+                        type="radio"
+                        label={
+                          <div>
+                            <AiOutlineCheck /> Anime
+                          </div>
+                        }
+                        name="style"
+                        value="vivid" // Default to 'vivid' for unsupported styles
+                        checked={selectedStyle === "vivid"}
+                        onChange={(e) => setSelectedStyle(e.target.value)}
+                      />
+                      <Form.Check
+                        type="radio"
+                        label={
+                          <div>
+                            <AiOutlineCheck /> Realistic
+                          </div>
+                        }
+                        name="style"
+                        value="vivid" // Default to 'vivid' for unsupported styles
+                        checked={selectedStyle === "vivid"}
+                        onChange={(e) => setSelectedStyle(e.target.value)}
+                      />
+                      <Form.Check
+                        type="radio"
+                        label={
+                          <div>
+                            <AiOutlineCheck /> Cyberpunk
+                          </div>
+                        }
+                        name="style"
+                        value="vivid" // Default to 'vivid' for unsupported styles
+                        checked={selectedStyle === "vivid"}
+                        onChange={(e) => setSelectedStyle(e.target.value)}
+                      />
+
+                    </IconContext.Provider>
                   </Form.Group>
                 </Card.Body>
               </Card>
               <Card className="mt-2 w-100">
                 <Card.Body>
-                  {/* <h3>Inspiration Text</h3> */}
                   <Form.Group className="d-flex">
                     <Form.Control
                       type="text"
                       ref={inputRef}
                       value={inspirationText}
                       placeholder="Let Your Imagination Work Here...."
-                      onChange={(e) =>
-                        handleInspirationTextChange(e.target.value)
-                      }
+                      onChange={(e) => setInspirationText(e.target.value)}
                       className="user-input"
                     />
                     <Button onClick={imageGenerator} variant="primary">
@@ -371,22 +337,6 @@ const ImageGenerator = () => {
         cartItems={cartItems}
         handleRemoveFromCart={handleRemoveFromCart}
       />
-
-      {/* {showModal && (
-        <div className="modal">
-          <div className="modal-content">
-            <span className="close" onClick={handleCloseModal}>
-              &times;
-            </span>
-            <h2>Items in Cart</h2>
-            <div className="cart-items">
-              {cartItems.map((item, index) => (
-                <img key={index} src={item} alt={`Item ${index + 1}`} />
-              ))}
-            </div>
-          </div>
-        </div>
-      )} */}
     </>
   );
 };
